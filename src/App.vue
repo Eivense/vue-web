98eacab3a5545956f3957b725596a3c7cec732f0
Eivense <hexplosives96@gmail.com>
2017-12-09 16:28:13 +0800

    update
    App.vue && main.css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app" >
-    <div class="head shadow">
-      <div class="container">
-        <div class="nav"></div>
-      </div>
+    <div class="head div-center shadow">
+        <div class="nav-left"></div>
+        <div class="nav-right">
+          <a class="nav-item">Home</a>
+          <a class="nav-item">bbbbbbbb</a>
+        </div>
     </div>
     <div class="profile">
-      <div style="margin-top: 8rem">
+      <div style="margin-top: 5rem">
         <div class="row div-center">
           <div class="col-md-8 col-md-offset-2">
             <img :src="'../static/image/cat.jpg'" class="img-circle center-block">
@@ -22,7 +24,23 @@
         </div>
       </div>
     </div>
-  </div>
+    <div class="main">
+      <div class="row div-center">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="container">
+              <div class="box"></div>
+              <div class="box"></div>
+              <div class="box"></div>
+            </div>
+          <div class="container">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -55,16 +73,27 @@ export default {
 
 <style>
   .profile{
-    padding: 50px;
+    padding: 20px  50px 15px 50px;
   }
   .head{
     height: 52px;
-  }
-  .div-center{
-    margin:auto;
+    width:1400px;
+    display: flex;
   }
   p{
     margin: 2rem;
+  }
+  .main{
+    padding: 15px  50px 15px 50px;
+  }
+  .box{
+    height:220px;
+    width: 300px;
+    border-radius: 5px;
+    color:#7a7a7a;
+    box-shadow: 0 2px 1px #7a7a7a,0 0 0 1px #7a7a7a;
+    border: 1px solid #7a7a7a;
+    margin: auto;
   }
 
 </style>
